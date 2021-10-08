d9808986346694d05b6427abff91abad92cbac2f
Koichi Shiraishi <zchee.io@gmail.com>
2021-10-08 19:54:58 +0900

    main: fix flags and some if conditions
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
-	"time"
 
 	"golang.org/x/tools/imports"
 )
@@ -55,9 +54,9 @@ func main() {
 }
 
 func run() error {
-	flag.Var(&flagPackages, "packages", "package name")
-	flag.StringVar(&flagModule, "module", "", "module name")
-	flag.StringVar(&flagSrc, "src", ".", "src directory")
+	flag.Var(&flagPackages, "package", "comma separated copy stdlib packages")
+	flag.StringVar(&flagModule, "module", "", "module import path")
+	flag.StringVar(&flagSrc, "src", runtime.GOROOT(), "src directory")
 	flag.StringVar(&flagDist, "dst", ".", "dist directory")
 	flag.Parse()
 
@@ -73,15 +72,16 @@ func run() error {
 		var packages []*Package
 		for _, listPkg := range listPkgs {
 			if _, err := os.Stat(listPkg.Dir); err != nil && os.IsNotExist(err) {
-				fmt.Printf("[WARN]: %s is not exists, continue\n", listPkg.Dir)
-
+				if listPkg.Dir != "" {
+					fmt.Printf("[WARN]: %s is not exists, continue\n", listPkg.Dir)
+				}
 				continue
 			}
 
 			packages = append(packages, listPkg)
 			for _, imp := range listPkg.Imports {
 				switch {
-				case strings.HasPrefix(imp, filepath.Join("cmd", "internal")), strings.HasPrefix(imp, "internal"):
+				case strings.HasPrefix(imp, "cmd"), strings.HasPrefix(imp, "internal"):
 					subPkgs, err := listPackages(ctx, flagSrc, imp)
 					if err != nil {
 						return err
@@ -115,6 +115,55 @@ func run() error {
 	return nil
 }
 
+// listPackages is a wrapper for 'go list -json -e', which can take arbitrary
+// environment variables and arguments as input. The working directory can be
+// fed by adding $PWD to env; otherwise, it will default to the current
+// directory.
+//
+// Since -e is used, the returned error will only be non-nil if a JSON result
+// could not be obtained. Such examples are if the Go command is not installed,
+// or if invalid flags are used as arguments.
+//
+// Errors encountered when loading packages will be returned for each package,
+// in the form of PackageError. See 'go help list'.
+func listPackages(ctx context.Context, src string, args ...string) (pkgs []*Package, finalErr error) {
+	goArgs := append([]string{"list", "-json", "-e"}, args...)
+	cmd := exec.CommandContext(ctx, "go", goArgs...)
+	cmd.Env = append(os.Environ(), []string{"PWD=" + src}...)
+	cmd.Dir = src
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	var stderrBuf bytes.Buffer
+	cmd.Stderr = &stderrBuf
+	defer func() {
+		if finalErr != nil && stderrBuf.Len() > 0 {
+			// TODO: wrap? but the format is backwards, given that
+			// stderr is likely multi-line
+			finalErr = fmt.Errorf("%w\n%s", finalErr, stderrBuf.Bytes())
+		}
+	}()
+
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	dec := json.NewDecoder(stdout)
+	for dec.More() {
+		var pkg Package
+		if err := dec.Decode(&pkg); err != nil {
+			return nil, err
+		}
+		pkgs = append(pkgs, &pkg)
+	}
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+
+	return pkgs, nil
+}
+
 func copyInternal(pkg *Package) error {
 	for _, path := range sourceFiles(pkg) {
 		// zbootstrap.go is created by bootstrap
@@ -136,7 +185,6 @@ func copyInternal(pkg *Package) error {
 			if strings.HasPrefix(pkg.Dir, imppath) {
 				dstpath := strings.ReplaceAll(pkg.Dir, imppath, fixpath)
 
-				fmt.Printf("dstpath: %#v\n", strings.TrimPrefix(dstpath, gorootSrc))
 				if err := writeFile(strings.TrimPrefix(dstpath, gorootSrc), name, body); err != nil {
 					return err
 				}
@@ -200,137 +248,4 @@ func writeFile(dir, name, body string) error {
 	}
 
 	return nil
-}
-
-// listPackages is a wrapper for 'go list -json -e', which can take arbitrary
-// environment variables and arguments as input. The working directory can be
-// fed by adding $PWD to env; otherwise, it will default to the current
-// directory.
-//
-// Since -e is used, the returned error will only be non-nil if a JSON result
-// could not be obtained. Such examples are if the Go command is not installed,
-// or if invalid flags are used as arguments.
-//
-// Errors encountered when loading packages will be returned for each package,
-// in the form of PackageError. See 'go help list'.
-func listPackages(ctx context.Context, src string, args ...string) (pkgs []*Package, finalErr error) {
-	goArgs := append([]string{"list", "-json", "-e"}, args...)
-	cmd := exec.CommandContext(ctx, "go", goArgs...)
-	cmd.Env = append(os.Environ(), []string{"PWD=" + src}...)
-	cmd.Dir = src
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-	var stderrBuf bytes.Buffer
-	cmd.Stderr = &stderrBuf
-	defer func() {
-		if finalErr != nil && stderrBuf.Len() > 0 {
-			// TODO: wrap? but the format is backwards, given that
-			// stderr is likely multi-line
-			finalErr = fmt.Errorf("%w\n%s", finalErr, stderrBuf.Bytes())
-		}
-	}()
-
-	if err := cmd.Start(); err != nil {
-		return nil, err
-	}
-	dec := json.NewDecoder(stdout)
-	for dec.More() {
-		var pkg Package
-		if err := dec.Decode(&pkg); err != nil {
-			return nil, err
-		}
-		pkgs = append(pkgs, &pkg)
-	}
-	if err := cmd.Wait(); err != nil {
-		return nil, err
-	}
-
-	return pkgs, nil
-}
-
-type Module struct {
-	Path      string       // module path
-	Version   string       // module version
-	Versions  []string     // available module versions (with -versions)
-	Replace   *Module      // replaced by this module
-	Time      *time.Time   // time version was created
-	Update    *Module      // available update, if any (with -u)
-	Main      bool         // is this the main module?
-	Indirect  bool         // is this module only an indirect dependency of main module?
-	Dir       string       // directory holding files for this module, if any
-	GoMod     string       // path to go.mod file used when loading this module, if any
-	GoVersion string       // go version used in module
-	Error     *ModuleError // error loading module
-}
-
-type ModuleError struct {
-	Err string // the error itself
-}
-
-type Package struct {
-	Dir           string   // directory containing package sources
-	ImportPath    string   // import path of package in dir
-	ImportComment string   // path in import comment on package statement
-	Name          string   // package name
-	Doc           string   // package documentation string
-	Target        string   // install path
-	Shlib         string   // the shared library that contains this package (only set when -linkshared)
-	Goroot        bool     // is this package in the Go root?
-	Standard      bool     // is this package part of the standard Go library?
-	Stale         bool     // would 'go install' do anything for this package?
-	StaleReason   string   // explanation for Stale==true
-	Root          string   // Go root or Go path dir containing this package
-	ConflictDir   string   // this directory shadows Dir in $GOPATH
-	BinaryOnly    bool     // binary-only package (no longer supported)
-	ForTest       string   // package is only for use in named test
-	Export        string   // file containing export data (when using -export)
-	Module        *Module  // info about package's containing module, if any (can be nil)
-	Match         []string // command-line patterns matching this package
-	DepOnly       bool     // package is only a dependency, not explicitly listed
-
-	// Source files
-	GoFiles         []string // .go source files (excluding CgoFiles, TestGoFiles, XTestGoFiles)
-	CgoFiles        []string // .go source files that import "C"
-	CompiledGoFiles []string // .go files presented to compiler (when using -compiled)
-	IgnoredGoFiles  []string // .go source files ignored due to build constraints
-	CFiles          []string // .c source files
-	CXXFiles        []string // .cc, .cxx and .cpp source files
-	MFiles          []string // .m source files
-	HFiles          []string // .h, .hh, .hpp and .hxx source files
-	FFiles          []string // .f, .F, .for and .f90 Fortran source files
-	SFiles          []string // .s source files
-	SwigFiles       []string // .swig files
-	SwigCXXFiles    []string // .swigcxx files
-	SysoFiles       []string // .syso object files to add to archive
-	TestGoFiles     []string // _test.go files in package
-	XTestGoFiles    []string // _test.go files outside package
-
-	// Cgo directives
-	CgoCFLAGS    []string // cgo: flags for C compiler
-	CgoCPPFLAGS  []string // cgo: flags for C preprocessor
-	CgoCXXFLAGS  []string // cgo: flags for C++ compiler
-	CgoFFLAGS    []string // cgo: flags for Fortran compiler
-	CgoLDFLAGS   []string // cgo: flags for linker
-	CgoPkgConfig []string // cgo: pkg-config names
-
-	// Dependency information
-	Imports      []string          // import paths used by this package
-	ImportMap    map[string]string // map from source import to ImportPath (identity entries omitted)
-	Deps         []string          // all (recursively) imported dependencies
-	TestImports  []string          // imports from TestGoFiles
-	XTestImports []string          // imports from XTestGoFiles
-
-	// Error information
-	Incomplete bool            // this package or a dependency has an error
-	Error      *PackageError   // error loading package
-	DepsErrors []*PackageError // errors loading dependencies
-}
-
-type PackageError struct {
-	ImportStack []string // shortest path from package named on command line to this one
-	Pos         string   // position of error (if present, file:line:col)
-	Err         string   // the error itself
 }
